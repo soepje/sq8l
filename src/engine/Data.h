f2ad30a0fb90169ed9f4fd889d95c7331c443bed
soepje <soepje@users.noreply.github.com>
2021-01-30 12:54:10 +0100

    Add missing doc data

--- src/engine/Data.h
+++ src/engine/Data.h
@@ -3,7 +3,7 @@
 extern unsigned char /* 0x494318 */ docSampleZones[75][16];
 extern unsigned char /* 0x4947c8 */ docWaveParameters[151][4];
 extern unsigned int /* 0x4c0278 */ docVolumeCurve2[2050];
-extern float /* 0x4c127c */ docVolumeCurve[64];
+extern float /* 0x4c127c */ docVolumeCurve[256];
 extern unsigned char /* 0x494e4c */ docPackedWaves[0x2b429];
 extern char /* 0x4c1688 */ envShapingCurves[8][256];
 extern unsigned char /* 0x494dcc */ envExponentialVelocityCurve[128];
